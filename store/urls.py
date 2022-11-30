c0624ca56e499bc7b2a5bfa6e6c4e968337e3fb6
Haruna Njie <harunanjie@hotmail.com>
2022-11-30 07:48:06 -0500

    build cart system using sessions

--- store/urls.py
+++ store/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path("detail/<int:pk>/", views.ProductDetailView.as_view(), name="detail"),
     path("list/", views.ProductListView.as_view(), name="list"),
+    path("<int:product_id>/cart/", views.AddToCartView.as_view(), name="cart"),
+    path("cart-items/", views.CartView.as_view(), name="cart_view"),
     # path("signup/", UserSignupView.as_view(), name="signup"),
     # path("address/", AddressView.as_view(), name="address"),
 ]
--- store/views.py
+++ store/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, DetailView, ListView
+from django.views.generic import TemplateView, DetailView, ListView, TemplateView
+from django.views import View
 from .models import Product
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -42,6 +45,61 @@ class ProductListView(ListView):
         return queryset
 
 
+class AddToCartView(View):
+    def get(self, request, *args, **kwargs):
+        product_id = kwargs["product_id"]
+        product = Product.objects.get(pk=product_id)
+        recently_viewed_products = None
+
+        if "recently_added" in request.session:
+            # if product_id in request.session["recently_added"]:
+            #     request.session["recently_viewed"].remove(product_id) # later for removing item from cart
+
+            # products = Product.objects.filter(pk__in=request.session["recently_added"])
+            # recently_viewed_products = sorted(
+            #     products, key=lambda x: request.session["recently_added"].index(x.id)
+            # )
+            request.session["recently_added"].insert(0, product_id)
+            # if len(request.session["recently_viewed"]) > 5:
+            #     request.session["recently_viewed"].pop()
+        else:
+            request.session["recently_added"] = [product_id]
+            # return HttpResponseRedirect(reverse_lazy("products:cart"))
+
+        request.session.modified = True
+
+        # context = {
+        #     "product": product,
+        #     "recently_viewed_products": recently_viewed_products,
+        # }
+        # return render(request, "store/cart.html", context)
+        return HttpResponseRedirect(reverse_lazy("products:cart_view"))
+
+
+class CartView(TemplateView):
+    template_name = "store/cart.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        products = None
+        recently_added_products = None
+        if "recently_added" in self.request.session:
+            products = Product.objects.filter(
+                pk__in=self.request.session["recently_added"]
+            )
+        else:
+            self.request.session["recently_added"] = []
+            return context
+        if products:
+            recently_added_products = sorted(
+                products,
+                key=lambda x: self.request.session["recently_added"].index(x.id),
+            )
+        # request.session["recently_viewed"].insert(0, product_id)
+        context["products"] = recently_added_products
+        return context
+
+
 """
 on product detail page, click add to cart and take product id and push it 
 onto a cart list in a session variable.
@@ -51,6 +109,8 @@ iterate over them and render them on the page by looking them up in the db.
 
 When user clicks checkout, grab cart items from session variable and create an order 
 item from the Orders model.
+
+
 """
 # class ProductSearchListView(ListView):
 #     model = Product
